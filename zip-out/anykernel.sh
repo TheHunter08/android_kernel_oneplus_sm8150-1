3b1dec56154e3e325a416d011d652420ee5efddf
Patrontrox <sgn_juanlu@protonmail.com>
2019-12-12 20:24:34 +0100

    Update zip-out

--- zip-out/anykernel.sh
+++ zip-out/anykernel.sh
@@ -4,22 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Revolution Kernel by Revolution Techs Team
-do.devicecheck=1
+kernel.string=Revolution Kernel by @Patrontrox
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=guacamole
-device.name2=OnePlus7Pro
+device.name1=
+device.name2=
 device.name3=
 device.name4=
 device.name5=
 supported.versions=
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -30,8 +31,17 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## begin vendor changes
+#mount -o rw,remount -t auto /vendor >/dev/null;
+
+#cp -rf /tmp/anykernel/patch/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh;
+#set_perm 0 0 0755 /vendor/bin/init.qcom.post_boot.sh;
+
+#cp -rf /tmp/anykernel/patch/msm_irqbalance.conf /vendor/etc/msm_irqbalance.conf;
+#set_perm 0 0 0644 /vendor/etc/msm_irqbalance.conf;
 
 
 ## AnyKernel install
@@ -39,7 +49,63 @@ dump_boot;
 
 # begin ramdisk changes
 
+# Add skip_override parameter to cmdline so user doesn't have to reflash Magisk
+if [ -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
+
+
+if mountpoint -q /data; then
+  # Optimize F2FS extension list (@arter97)
+  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
+    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
+
+    # Skip update if our list is already active
+    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
+      echo "Extension list up-to-date: $list_path"
+      continue
+    fi
+
+    ui_print "  • Optimizing F2FS extension list"
+    echo "Updating extension list: $list_path"
+
+    echo "Clearing extension list"
+
+    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
+    list_len="$(cat $list_path | wc -l)"
+    cold_count="$((list_len - hot_count))"
+
+    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
+    hot_list="$(tail -n$cold_count $list_path)"
+
+    for ext in $cold_list; do
+      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
+    done
+
+    for ext in $hot_list; do
+      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
+    done
+
+    echo "Writing new extension list"
+
+    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
+      [ ! -z $ext ] && echo "[c]$ext" > $list_path
+    done
+
+    for ext in $(cat $home/f2fs-hot.list); do
+      [ ! -z $ext ] && echo "[h]$ext" > $list_path
+    done
+  done
+fi
+
 # end ramdisk changes
+    
+	ui_print " "; ui_print "Flashing Revolution_Kernel. Have a good day!";
+	ui_print " "; ui_print "Please make sure to always run the latest companion magisk module!";
+
 
 write_boot;
 ## end install
